881b88e7e8331bb04ff8b5b8f952fff4ef4e9904
Olapu Ajay <120544151+olapuajay@users.noreply.github.com>
2025-02-05 10:05:20 +0530

    Time: 8 ms (47.16%) | Memory: 40.3 MB (63.35%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -4,8 +4,8 @@ class Solution {
         return res;
     }
     public int fibonacci(int n) {
-        if(n <= 1) return n;
-        int ans = fibonacci(n - 1) + fibonacci(n - 2); 
+        if(n < 2) return n;
+        int ans = fibonacci(n - 1) + fibonacci(n - 2);
         return ans;
     }
 }
