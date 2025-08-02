ff8f99a17ee0dcd8eb89097f6be6c0d344e0711a
Ajay <120544151+olapuajay@users.noreply.github.com>
2025-08-02 19:18:06 +0530

    Time: 1 ms (92.05%) | Memory: 40.5 MB (73.80%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int countOperations(int num1, int num2) {
-        return helper(num1, num2, 0);
-    }
-    private int helper(int num1, int num2, int oper) {
-        if(num1 == 0 || num2 == 0) return oper;
-
-        if(num1 >= num2) {
-            return helper(num1 - num2, num2, oper + 1);
-        } else {
-            return helper(num1, num2 - num1, oper + 1);
-        }
-    }
     // public int countOperations(int num1, int num2) {
-    //     int oper = 0;
-    //     while(num1 > 0 && num2 > 0) {
-    //         if(num1 >= num2) {
-    //             num1 = num1 - num2;
-    //         } else {
-    //             num2 = num2 - num1;
-    //         }
-    //         oper++;
-    //     }
-    //     return oper;
+    //     return helper(num1, num2, 0);
     // }
+    // private int helper(int num1, int num2, int oper) {
+    //     if(num1 == 0 || num2 == 0) return oper;
+
+    //     if(num1 >= num2) {
+    //         return helper(num1 - num2, num2, oper + 1);
+    //     } else {
+    //         return helper(num1, num2 - num1, oper + 1);
+    //     }
+    // }
+    public int countOperations(int num1, int num2) {
+        int oper = 0;
+        while(num1 > 0 && num2 > 0) {
+            if(num1 >= num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            oper++;
+        }
+        return oper;
+    }
 }
